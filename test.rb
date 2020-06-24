84da31805790b9041b322efd39d2c82abc13ab6d
Charlie Barth <charliepbarth@gmail.com>
2020-06-24 21:03:59 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,7 +3,4 @@ def reverse_each_word(sentence)
   array = sentence.split
  puts array
 end
-.each do |word|
-    count = array.index("#{word}")
-    array[count].reverse!
-  end
+reverse_each_word("Hello there, and how are you?")
